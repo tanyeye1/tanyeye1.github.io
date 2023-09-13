5b1c76d9781ab33f400c70de511ab5b911a3ef34
tanyeye1 <1552467926@qq.com>
2023-09-13 11:00:53 +0800

    Site updated: 2023-09-13 11:00:52

--- photos/ins.js
+++ photos/ins.js
@@ -130,12 +130,6 @@
                 </a>\
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
             </figure>';
-          // liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
-          //       <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-          //         <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/assets/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
-          //       </a>\
-          //       <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
-          //   </figure>';
         }
         ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em></h1>\
         <ul class="img-box-ul">' + liTmpl + '</ul>\
